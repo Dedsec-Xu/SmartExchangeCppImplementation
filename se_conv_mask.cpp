9205f8f6e5e43a6b6ac35b1d3d0be76ba45c810e
Dedsec-Xu <xld1998615@vip.qq.com>
2019-08-27 15:11:11 -0500

    2d

--- se_conv_mask.cpp
+++ se_conv_mask.cpp
@@ -1,3 +1,10 @@
+//
+//  se_conv_mask.cpp
+//  
+//
+//  Created by LIANGDONG XU on 8/27/19.
+//  Copyright © 2019 LIANGDONG XU. All rights reserved.
+//
 #include <iostream>
 using namespace std;
 int main()
@@ -5,14 +12,14 @@ int main()
 
     return 0;
 }
-
+/* This is a 2d version of the problem
 float * reshape_3 (float * W, int M, int C)//im guessing W is the pointer of row Wi
 {
     float (*Mat_p)[3];// p is the reshaped matrix
     int Mat_Col = C / 3;// the row count of mat
     int Mat_Zero = C % 3;// the padded zero count
     int iter;
-    /*requires memallocate here*/
+    //requires memallocate here
     for(iter = 0; iter < 3 * (Mat_Col + 1); iter++)
     {
         if(iter < C)
@@ -26,5 +33,5 @@ float * reshape_3 (float * W, int M, int C)//im guessing W is the pointer of row
     }
     return Mat_p;
 }
-
+*/
  
