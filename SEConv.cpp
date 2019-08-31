4d0b56cea3e5435d6c0195b7d6f2df26ab4b57af
Wilson Xu <1009181441@qq.com>
2019-08-31 01:55:34 -0500

    Revert "Revert "update""
    This reverts commit 9ff2bdd3bdc81ef07eeca6d040cd3f05ba5e9f13.

--- SEConv.cpp
+++ SEConv.cpp
@@ -419,4 +419,64 @@ fixed sparsify_and_quantize_C(fixed qC[][BF_CE_2][BF_CE_3])
 		}
 	}
 }
-	
+
+int bmm(int input1[][buffersize_x][buffersize_y], int input2[][buffersize_y][buffersize_r], 
+	int output[][buffersize_x][buffersize_r], int input1_dim[], int input2_dim[], int output_dim[])
+{
+	int d = input1_dim[0];
+	int m = input1_dim[1];
+	int r = input1_dim[2];
+	int a[m][n];
+	int b[n][l];
+	int c[m][l];
+	for (int d = 0; d <= dim; d++) // d= dimension
+	{
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				a[m][n] = input1[d][i][j];
+			}
+		}
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				b[m][n] = input2[d][i][j];
+			}
+		}
+
+		///initial the computed martix/
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				c[i][j] = 0;
+			}
+		}
+		///compute the martix/
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					c[i][j] = c[i][j] + (a[i][k] * b[k][j]);
+				}
+			}
+		}
+		/// display the martix/
+		// cout << endl << endl << "result："<< endl << endl;
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				// cout << c[i][j] << "\t";
+				output[d][i][j] = c[i][j];
+			}
+			// cout << endl << endl;
+		}
+	}
+	return 0;
+}
