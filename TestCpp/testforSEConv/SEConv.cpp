0ab7ec8595a15c31ef1ca5846db50f3898b2c4a5
Dedsec-Xu <xld1998615@vip.qq.com>
2019-09-04 23:19:08 -0500

    reshapeBC IS SO SIMPLE!

--- TestCpp/testforSEConv/SEConv.cpp
+++ TestCpp/testforSEConv/SEConv.cpp
@@ -295,7 +295,7 @@ int SEforward(bool bn, bool relu, int batch_size, int ch_in,
 	return 1;
 }
 
-float SEbackward(float Ce_buffer[][BF_CE_2][BF_CE_3], float loss, float max_C, float min_C, float qC_buffer[][BF_CE_1][BF_CE_1], int size_C_dim[], float Learning_Rate, float mask_data_buffer[][BF_CE_1][BF_CE_1], float threshold)
+float SEbackward(float weight_grad[], float Ce_buffer[][BF_CE_2][BF_CE_3], float loss, float max_C, float min_C, float qC_buffer[][BF_CE_1][BF_CE_1], int size_C_dim[], float Learning_Rate, float mask_data_buffer[][BF_CE_1][BF_CE_1], float threshold)
 {
 
 	float dC_buffer[BF_CE_1][BF_CE_2][BF_CE_3];
@@ -574,69 +574,18 @@ int reshape(float input1[][buffersize_x][buffersize_y], float output[][buffersiz
 }
 
 
-int reshapeBC(float input1[][buffersize_x][buffersize_y], float output[][buffersize_x][buffersize_y][buffersize_y], int inputdim[], int dim_1, int dim_2, int dim_3, int dim_4)
+int reshapeBC(float weight_grad[], int size_C_dim[]，float BC[][BF_CE_2][BF_CE_3])
 {
-    int input_d1, input_d2, input_d3, input_d, input_d1X2, in_iter_1, in_iter_2, in_iter_3;
-    input_d1 = inputdim[0];
-    input_d2 = inputdim[1];
-    input_d3 = inputdim[2];
-    in_iter_1 = 0;
-    in_iter_2 = 0;
-    in_iter_3 = 0;
-    input_d1X2 = input_d1 * input_d2;
-    int Reshape_Size = input_d1 * input_d2 * input_d3;
-    int break_flag = 0;
-    float Reshape_Buffer[Reshape_Buffer_Size];
-    int current_iter;
-    if(dim_2 == -1)
-    {
-        dim_2 = ((Reshape_Size / dim_1) / dim_3) / dim_4;
-    }
-    for(int iter_1 = 0; iter_1 < dim_1; iter_1++)
-    {
-        for(int iter_2 = 0; iter_2 < dim_2; iter_2++)
-        {
-            for(int iter_3 = 0; iter_3 < dim_3; iter_3++)
-            {
-                for(int iter_4 = 0; iter_4 < dim_4; iter_4++)
-                {
-                    output[iter_1][iter_2][iter_3][iter_4] = input1[in_iter_1][in_iter_2][in_iter_3];
-                    current_iter = iter_1 * dim_1 + iter_2 * dim_2 + iter_3 * dim_3 + iter_4 * dim_4;
-                    // cout << "output" << "[" << iter_1 << "]" << "[" << iter_2 << "]" << "[" << iter_3 << "]" << "[" << iter_4 << "]" << "=" << output[iter_1][iter_2][iter_3][iter_4] << endl;
-                    // cout << "input1" << "[" << in_iter_1 << "]" << "[" << in_iter_2 << "]" << "[" << in_iter_3 << "]" << "=" << input1[in_iter_1][in_iter_2][in_iter_3] << endl;
-                    in_iter_3 += 1;
-                    if(in_iter_3>=input_d3)
-                    {
-                        in_iter_3 = 0;
-                        in_iter_2 += 1;
-                        if(in_iter_2>=input_d2)
-                        {
-                            in_iter_2 = 0;
-                            in_iter_1 += 1;
-                            if(in_iter_1>=input_d1)
-                            {
-                                break_flag = 1;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if(break_flag == 1)
-                {
-                    break;
-                }
-            }
-            if(break_flag == 1)
-            {
-                break;
-            }
-        }
-        if(break_flag == 1)
-        {
-            break;
-        }
-    }
-	return dim_2;
+	for(int iter_1 = 0; iter_1 < size_C_dim[0]; iter_1++)
+	{
+		for(int iter_2 = 0; iter_2 < size_C_dim[1]; iter_2++)
+		{
+			for(int iter_3 = 0; iter_3 < size_C_dim[2]; iter_3++)
+			{
+				BC[iter_1][iter_2][iter_3] = weight_grad[iter_1 * size_C_dim[1] * size_C_dim[2] + iter_2 * size_C_dim[2] + iter_3];
+			}
+		}
+	}
 }
 
 //int step(int weight_decay, float momentum, float dampening, bool nesterov,int size_C_dim[])
